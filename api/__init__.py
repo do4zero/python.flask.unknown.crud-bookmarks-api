363030c55de1281d9d5482211a518abb360665ba
zero <doing4zero@gmail.com>
2023-01-22 23:08:40 +0700

    register endpoint

--- api/__init__.py
+++ api/__init__.py
@@ -3,22 +3,27 @@ import os
 from api.auth import auth
 from api.bookmars import bookmarks
 from api.database import db
+from flask_cors import CORS
 
-def create_app(test_config = None):
+
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    
+
     if test_config is None:
         app.config.from_mapping(
-            SECRET_KEY = os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False
         )
     else:
         app.config.from_mapping(test_config)
-        
+
     db.app = app
     db.init_app(app)
-    
+
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
-    
+
+    CORS(app)
+
     return app
